312fec70acf3833056c3e90b6337679a1d5453eb
Tanimodori <unknowner2014@gmail.com>
2018-04-02 23:30:59 +0800

    Minor fix

---
+++ archive/Replacer-Gordon/README.md
@@ -0,0 +1,9 @@
+#Replacer
+##Usage
+	- Put "GregTech.lang" under this folder
+	- Run counter.py to get a list of words you need to translate
+	- Open "pending_dictionary.dict"
+	- Put translation after "=" for each words. **Note: Each line can only have one translation! Space is counted in!
+	- Rename the "pending_dictionary.dict" you just finished to "dictionary.dict"
+	- Run Replacer.py
+	- Output file is "Gregtech_converted.lang"
--- source/gregtech/api/build.py
+++ source/gregtech/api/build.py
@@ -10,6 +10,7 @@ def build_lang():
     with open('config/division.yml', 'r', encoding='utf-8') as config_file:
         config = yaml.load(config_file)
         output = {}
+
         for item in config:
             group_name = item['name']
             with open('lang/'+group_name+'/zh_cn.lang', 'r', encoding='utf-8') as lang_file:
@@ -17,8 +18,11 @@ def build_lang():
                     lang_file, lambda x: x.strip().startswith('S:'))
                 output.update(lang)
         with open('GregTech.lang', 'w', encoding='utf-8') as output_file:
+            output_file.write(
+                "# Configuration file\n\nenablelangfile {\n    B:UseThisFileAsLanguageFile=true\n}\n\n\nlanguagefile {\n")
             for k, v in sorted(output.items(), key=lambda x: x[0]):
-                output_file.write('{0}={1}\n'.format(k, v))
+                output_file.write('    {0}={1}\n'.format(k, v))
+            output_file.write("\n}")
 
     print('Building completed successfully')
 
