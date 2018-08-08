36c342a87a33ca21642e81898fa37a13221de22f
Tanimodori <unknowner2014@gmail.com>
2018-08-09 00:44:25 +0800

    Rename the first arg in get_item_by_key function to make it more clear

--- source/replacer/glossary.py
+++ source/replacer/glossary.py
@@ -3,22 +3,23 @@
 import re
 
 
-def get_item_by_key(self, name, key):
+def get_item_by_key(glossary, name, key):
     """Get the item with the corresponding name and key
 
     Args:
+        glossary (dict): a dict containing original words and their corresponding translated words 
         name (str): name you want to search
-        key (str): key of your name,
+        key (str): key of your name
 
     Returns:
         you value, None if not found
 
     Examples:
         Iron_zh_cn=glossary.get_item_by_key('Iron','gt.material') """
-    if name in self.keys():
-        if type(self[name]) is str:
-            return self[name]
+    if name in glossary.keys():
+        if type(glossary[name]) is str:
+            return glossary[name]
         else:
-            return next((x['value'] for x in self[name] if re.match(x['regex'], key)), None)
+            return next((x['value'] for x in glossary[name] if re.match(x['regex'], key)), None)
     else:
         return None
